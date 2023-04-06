af0decd726263714c7af87c729e852d9fc26e280
Jayden <jaydendncn2019@gmail.com>
2023-04-06 13:26:09 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/calculatorremix/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.calculatorremix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.calculatorremix.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
